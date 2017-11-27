5d1d8349ba24a04a889e7eee030d2558f4623df9
Korek302 <jacek_blady@interia.pl>
2017-11-27 11:40:00 +0100

    build

---
+++ dotNETl7z1/Fraction.cs
@@ -0,0 +1,168 @@
+﻿using System;
+
+//Fraction.cs
+
+/// <summary>
+/// Struktura Fraction stanowi reprezentacje ułameka zwykłego.
+/// Struktura Fraction udostępnia typowe dziaania na ułamkach.
+/// </summary>
+struct Fraction
+{
+    /// <summary>
+    /// Pole x (int) - reprezentuje licznik ułamka.
+    /// </summary>
+    int x;
+
+    /// <summary>
+    /// Pole y (int) - reprezentuje mianownik ułamka.
+    /// </summary>
+    int y;
+
+    /// <summary>
+    /// Publiczny konstruktor parametryczny.
+    /// Przypisuje polom x i y zadane wartości przy czym sprawdza czy mianownik jest równy 0.
+    /// </summary>
+    /// <param name="x"> Zadana wartość licznika (int) </param>
+    /// <param name="y"> Zadana wartość mianownika (int) </param>
+    public Fraction(int x, int y)
+    {
+        this.x = x;
+        if (y == 0)
+        {
+            throw new DivideByZeroException();
+        }
+        this.y = y;
+    }
+
+    /// <summary>
+    /// Przeciążony operator dodawania "+".
+    /// Dodaje do siebie 2 zadane ułamki.
+    /// </summary>
+    /// <param name="a"> Pierwszy składnik sumy (Fraction) </param>
+    /// <param name="b"> Drugi składnik sumy (Fraction) </param>
+    /// <returns> Suma ułamków (Fraction) </returns>
+    public static Fraction operator +(Fraction a, Fraction b)
+    {
+        return new Fraction(a.x * b.y + b.x * a.y, a.y * b.y);
+    }
+
+    /// <summary>
+    /// Przeciążony operator odejmowania "-".
+    /// Odejmuje pierwszy zadany ułamek od drugiego.
+    /// </summary>
+    /// <param name="a"> Odjemna różnicy (Fraction) </param>
+    /// <param name="b"> Odjemnik różnicy (Fraction) </param>
+    /// <returns> Róznica ułamków (Fraction) </returns>
+    public static Fraction operator -(Fraction a, Fraction b)
+    {
+        return new Fraction(a.x * b.y - b.x * a.y, a.y * b.y);
+    }
+
+    /// <summary>
+    /// Przeciążony operator mnożenia "*".
+    /// Mnoży 2 zadane ułamki.
+    /// </summary>
+    /// <param name="a"> Pierwszy czynnik mnożenia (Fraction) </param>
+    /// <param name="b"> Drugi czynnik mnożenia (Fraction) </param>
+    /// <returns> Iloczyn ułamków (Fraction) </returns>
+    public static Fraction operator *(Fraction a, Fraction b)
+    {
+        return new Fraction(a.x * b.x, a.y * b.y);
+    }
+
+    /// <summary>
+    /// Przeciążony operator dzielenia "/".
+    /// Dzieli pierwszy zadany ułamek przez drugiego.
+    /// </summary>
+    /// <param name="a"> Dzielna ilorazu (Fraction) </param>
+    /// <param name="b"> Dzielnik ilorazu (Fraction) </param>
+    /// <returns> Iloraz ułamków (Fraction) </returns>
+    public static Fraction operator /(Fraction a, Fraction b)
+    {
+        return new Fraction(a.x * b.y, a.y * b.x);
+    }
+
+    /// <summary>
+    /// Funkcja simplify().
+    /// Skraca ułamek do najprostszej postaci.
+    /// </summary>
+    /// <returns> Ułamek w najprostszej postaci (Fraction) </returns>
+    public Fraction simplify()
+    {
+        return new Fraction(this.x / GCD(this.x, this.y), this.y / GCD(this.x, this.y));
+    }
+
+    /// <summary>
+    /// Funkcja GCD().
+    /// Funkcja pomcnicza funkcji simplify().
+    /// Oblicza największą wspólną wielkorotność dwóch liczb całkowitych.
+    /// </summary>
+    /// <param name="a"> Pierwsza zadana liczba całkowita (int) </param>
+    /// <param name="b"> Druga zadana liczba całkowita (int) </param>
+    /// <returns> Największa wspólna wielokrotność zadanych ułamków (int) </returns>
+    int GCD(int a, int b)
+    {
+        while (b > 0)
+        {
+            int rem = a % b;
+            a = b;
+            b = rem;
+        }
+        return a;
+    }
+    
+    /// <summary>
+    /// Funkcja toWhole().
+    /// Funkcja zamienia ułamek niewłaściwy na liczbę całkowitą z ułamkiem zwykłym
+    /// w postaci krotki.
+    /// </summary>
+    /// <returns> Krotka zawierająca na pierwszej pozycji liczbę całkowita,
+    /// a na drugiej ułamek właściwy(Tuple<int, Fraction>) </returns>
+    public Tuple<int, Fraction> toWhole()
+    {
+        int whole = 0;
+        int x = this.x;
+        while (x >= this.y)
+        {
+            whole++;
+            x -= this.y;
+        }
+        return new Tuple<int, Fraction>(whole, new Fraction(x, this.y));
+    }
+
+    /// <summary>
+    /// Niejawny operator konwersji z typu int na typ Fraction.
+    /// Zwraca ułamek zwykły, mający 1 w mianowniku i zadaną liczbę całkowitą w liczniku.
+    /// </summary>
+    /// <param name="x"> Zadana wartość (int) </param>
+    public static implicit operator Fraction(int x) => new Fraction(x, 1);
+
+    /// <summary>
+    /// Jawny operator konwersji z typu Fraction na typ int.
+    /// Zwraca liczbę całości zawartych w zdanym ułamku.
+    /// </summary>
+    /// <param name="f"> Zadany ułamek (Fraction) </param>
+    public static explicit operator int(Fraction f) => f.x / f.y;
+
+    /// <summary>
+    /// Getter struktury Fraction.
+    /// Zwraca wartość pola x.
+    /// </summary>
+    public int FracX => x;
+
+    /// <summary>
+    /// Getter struktury Fraction.
+    /// Zwraca wartość pola y.
+    /// </summary>
+    public int FracY => y;
+
+    /// <summary>
+    /// Funkcja toString().
+    /// Funkcja reprezentuje ułamek zwykły w postaci ciągu znaków.
+    /// </summary>
+    /// <returns> Ciąg znaków postaci [licznik]/[mianownik] (string) </returns>
+    public string toString()
+    {
+        return this.FracX + "/" + this.FracY;
+    }
+}
--- dotNETl7z1/Program.cs
+++ dotNETl7z1/Program.cs
@@ -1,12 +1,358 @@
 ﻿using System;
 
-namespace dotNETl7z1
+//Program.cs
+
+/// <summary>
+/// Klasa Program
+/// Zawiera metoda main, która uruchamia program.
+/// </summary>
+class Program
 {
-    class Program
+    /// <summary>
+    /// Punkt wejściowy aplikacji.
+    /// </summary>
+    /// <param name="args"> Lista argumentów wiersza poleceń. </param>
+    public static void Main()
     {
-        static void Main(string[] args)
+        Console.OutputEncoding = System.Text.Encoding.GetEncoding("UTF-8");
+        ConsoleKeyInfo cki;
+        do
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.WriteLine("Naciśnij: \n[1] - dodawanie\n[2] - odejmowanie\n[3] - mnozenie\n" +
+                "[4] - dzielenie\n[5] - skracanie\n[6] - zamiana na postać z liczbą całkowitą\n" +
+                "[7] - konwersja do inta\n[8] - konwersja z inta\n");
+            String choice = Console.ReadLine() + " ";
+
+            if (choice[0] == '1')
+            {
+                Console.WriteLine("Podaj licznik pierwszego ułamka: ");
+                string sx1 = Console.ReadLine();
+                int num;
+                while (!int.TryParse(sx1, out num) || sx1.Length == 0)
+                {
+                    Console.WriteLine("Nie podałeś liczby! Podaj jeszcze raz: ");
+                    sx1 = Console.ReadLine();
+                }
+                int ix1 = int.Parse(sx1);
+
+                Console.WriteLine("Podaj mianownik pierwszego ułamka: ");
+                string sy1 = Console.ReadLine();
+                while (!int.TryParse(sy1, out num) || sy1.Length == 0)
+                {
+                    Console.WriteLine("Nie podałeś liczby! Podaj jeszcze raz: ");
+                    sy1 = Console.ReadLine();
+                }
+                int iy1 = int.Parse(sy1);
+
+                Console.WriteLine("Podaj licznik drugiego ułamka: ");
+                string sx2 = Console.ReadLine();
+                while (!int.TryParse(sx2, out num) || sx2.Length == 0)
+                {
+                    Console.WriteLine("Nie podałeś liczby! Podaj jeszcze raz: ");
+                    sx2 = Console.ReadLine();
+                }
+                int ix2 = int.Parse(sx2);
+
+                Console.WriteLine("Podaj licznik drugiego ułamka: ");
+                string sy2 = Console.ReadLine();
+                while (!int.TryParse(sy2, out num) || sy2.Length == 0)
+                {
+                    Console.WriteLine("Nie podałeś liczby! Podaj jeszcze raz: ");
+                    sy2 = Console.ReadLine();
+                }
+                int iy2 = int.Parse(sy2);
+
+                if (iy1 != 0 && iy2 != 0)
+                {
+                    Fraction frac1 = new Fraction(ix1, iy1);
+                    Fraction frac2 = new Fraction(ix2, iy2);
+                    Fraction frac3 = frac1 + frac2;
+
+                    Console.WriteLine("{0} + {1} = {2}", frac1.toString(), frac2.toString(), frac3.toString());
+                }
+                else
+                {
+                    Console.WriteLine("Podałeś 0 w mianowniku!");
+                }
+            }
+            else if (choice[0] == '2')
+            {
+                Console.WriteLine("Podaj licznik pierwszego ułamka: ");
+                string sx1 = Console.ReadLine();
+                int num;
+                while (!int.TryParse(sx1, out num) || sx1.Length == 0)
+                {
+                    Console.WriteLine("Nie podałeś liczby! Podaj jeszcze raz: ");
+                    sx1 = Console.ReadLine();
+                }
+                int ix1 = int.Parse(sx1);
+
+                Console.WriteLine("Podaj mianownik pierwszego ułamka: ");
+                string sy1 = Console.ReadLine();
+                while (!int.TryParse(sy1, out num) || sy1.Length == 0)
+                {
+                    Console.WriteLine("Nie podałeś liczby! Podaj jeszcze raz: ");
+                    sy1 = Console.ReadLine();
+                }
+                int iy1 = int.Parse(sy1);
+
+                Console.WriteLine("Podaj licznik drugiego ułamka: ");
+                string sx2 = Console.ReadLine();
+                while (!int.TryParse(sx2, out num) || sx2.Length == 0)
+                {
+                    Console.WriteLine("Nie podałeś liczby! Podaj jeszcze raz: ");
+                    sx2 = Console.ReadLine();
+                }
+                int ix2 = int.Parse(sx2);
+
+                Console.WriteLine("Podaj licznik drugiego ułamka: ");
+                string sy2 = Console.ReadLine();
+                while (!int.TryParse(sy2, out num) || sy2.Length == 0)
+                {
+                    Console.WriteLine("Nie podałeś liczby! Podaj jeszcze raz: ");
+                    sy2 = Console.ReadLine();
+                }
+                int iy2 = int.Parse(sy2);
+
+                if (iy1 != 0 && iy2 != 0)
+                {
+                    Fraction frac1 = new Fraction(ix1, iy1);
+                    Fraction frac2 = new Fraction(ix2, iy2);
+                    Fraction frac3 = frac1 - frac2;
+
+                    Console.WriteLine("{0} - {1} = {2}", frac1.toString(), frac2.toString(), frac3.toString());
+                }
+                else
+                {
+                    Console.WriteLine("Podałeś 0 w mianowniku!");
+                }
+            }
+            else if (choice[0] == '3')
+            {
+                Console.WriteLine("Podaj licznik pierwszego ułamka: ");
+                string sx1 = Console.ReadLine();
+                int num;
+                while (!int.TryParse(sx1, out num) || sx1.Length == 0)
+                {
+                    Console.WriteLine("Nie podałeś liczby! Podaj jeszcze raz: ");
+                    sx1 = Console.ReadLine();
+                }
+                int ix1 = int.Parse(sx1);
+
+                Console.WriteLine("Podaj mianownik pierwszego ułamka: ");
+                string sy1 = Console.ReadLine();
+                while (!int.TryParse(sy1, out num) || sy1.Length == 0)
+                {
+                    Console.WriteLine("Nie podałeś liczby! Podaj jeszcze raz: ");
+                    sy1 = Console.ReadLine();
+                }
+                int iy1 = int.Parse(sy1);
+
+                Console.WriteLine("Podaj licznik drugiego ułamka: ");
+                string sx2 = Console.ReadLine();
+                while (!int.TryParse(sx2, out num) || sx2.Length == 0)
+                {
+                    Console.WriteLine("Nie podałeś liczby! Podaj jeszcze raz: ");
+                    sx2 = Console.ReadLine();
+                }
+                int ix2 = int.Parse(sx2);
+
+                Console.WriteLine("Podaj licznik drugiego ułamka: ");
+                string sy2 = Console.ReadLine();
+                while (!int.TryParse(sy2, out num) || sy2.Length == 0)
+                {
+                    Console.WriteLine("Nie podałeś liczby! Podaj jeszcze raz: ");
+                    sy2 = Console.ReadLine();
+                }
+                int iy2 = int.Parse(sy2);
+
+                if (iy1 != 0 && iy2 != 0)
+                {
+                    Fraction frac1 = new Fraction(ix1, iy1);
+                    Fraction frac2 = new Fraction(ix2, iy2);
+                    Fraction frac3 = frac1 * frac2;
+
+                    Console.WriteLine("{0} * {1} = {2}", frac1.toString(), frac2.toString(), frac3.toString());
+                }
+                else
+                {
+                    Console.WriteLine("Podałeś 0 w mianowniku!");
+                }
+            }
+            else if (choice[0] == '4')
+            {
+                Console.WriteLine("Podaj licznik pierwszego ułamka: ");
+                string sx1 = Console.ReadLine();
+                int num;
+                while (!int.TryParse(sx1, out num) || sx1.Length == 0)
+                {
+                    Console.WriteLine("Nie podałeś liczby! Podaj jeszcze raz: ");
+                    sx1 = Console.ReadLine();
+                }
+                int ix1 = int.Parse(sx1);
+
+                Console.WriteLine("Podaj mianownik pierwszego ułamka: ");
+                string sy1 = Console.ReadLine();
+                while (!int.TryParse(sy1, out num) || sy1.Length == 0)
+                {
+                    Console.WriteLine("Nie podałeś liczby! Podaj jeszcze raz: ");
+                    sy1 = Console.ReadLine();
+                }
+                int iy1 = int.Parse(sy1);
+
+                Console.WriteLine("Podaj licznik drugiego ułamka: ");
+                string sx2 = Console.ReadLine();
+                while (!int.TryParse(sx2, out num) || sx2.Length == 0)
+                {
+                    Console.WriteLine("Nie podałeś liczby! Podaj jeszcze raz: ");
+                    sx2 = Console.ReadLine();
+                }
+                int ix2 = int.Parse(sx2);
+
+                Console.WriteLine("Podaj licznik drugiego ułamka: ");
+                string sy2 = Console.ReadLine();
+                while (!int.TryParse(sy2, out num) || sy2.Length == 0)
+                {
+                    Console.WriteLine("Nie podałeś liczby! Podaj jeszcze raz: ");
+                    sy2 = Console.ReadLine();
+                }
+                int iy2 = int.Parse(sy2);
+
+                if (iy1 != 0 && iy2 != 0)
+                {
+                    Fraction frac1 = new Fraction(ix1, iy1);
+                    Fraction frac2 = new Fraction(ix2, iy2);
+                    Fraction frac3 = frac1 / frac2;
+
+                    Console.WriteLine("{0} / {1} = {2}", frac1.toString(), frac2.toString(), frac3.toString());
+                }
+                else
+                {
+                    Console.WriteLine("Podałeś 0 w mianowniku!");
+                }
+            }
+            else if (choice[0] == '5')
+            {
+                Console.WriteLine("Podaj licznik ułamka: ");
+                string sx1 = Console.ReadLine();
+                int num;
+                while (!int.TryParse(sx1, out num) || sx1.Length == 0)
+                {
+                    Console.WriteLine("Nie podałeś liczby! Podaj jeszcze raz: ");
+                    sx1 = Console.ReadLine();
+                }
+                int ix1 = int.Parse(sx1);
+
+                Console.WriteLine("Podaj mianownik ułamka: ");
+                string sy1 = Console.ReadLine();
+                while (!int.TryParse(sy1, out num) || sy1.Length == 0)
+                {
+                    Console.WriteLine("Nie podałeś liczby! Podaj jeszcze raz: ");
+                    sy1 = Console.ReadLine();
+                }
+                int iy1 = int.Parse(sy1);
+
+                if (iy1 != 0)
+                { 
+                    Fraction frac1 = new Fraction(ix1, iy1);
+                    Fraction frac2 = frac1.simplify();
+
+                    Console.WriteLine("{0} po skróceniu ma postać: {1}", frac1.toString(), frac2.toString());
+                }
+                else
+                {
+                    Console.WriteLine("Podałeś 0 w mianowniku!");
+                }
+            }
+            else if (choice[0] == '6')
+            {
+                Console.WriteLine("Podaj licznik ułamka: ");
+                string sx1 = Console.ReadLine();
+                int num;
+                while (!int.TryParse(sx1, out num) || sx1.Length == 0)
+                {
+                    Console.WriteLine("Nie podałeś liczby! Podaj jeszcze raz: ");
+                    sx1 = Console.ReadLine();
+                }
+                int ix1 = int.Parse(sx1);
+
+                Console.WriteLine("Podaj mianownik ułamka: ");
+                string sy1 = Console.ReadLine();
+                while (!int.TryParse(sy1, out num) || sy1.Length == 0)
+                {
+                    Console.WriteLine("Nie podałeś liczby! Podaj jeszcze raz: ");
+                    sy1 = Console.ReadLine();
+                }
+                int iy1 = int.Parse(sy1);
+
+                if (iy1 != 0)
+                {
+                    Fraction frac1 = new Fraction(ix1, iy1);
+                    Tuple<int, Fraction> outcome = frac1.toWhole();
+
+                    Console.WriteLine("{0} po wyciągnięciu całości ma postać: {1}", frac1.toString(), 
+                        outcome.Item1 + " " + outcome.Item2.toString());
+                }
+                else
+                {
+                    Console.WriteLine("Podałeś 0 w mianowniku!");
+                }
+            }
+            else if (choice[0] == '7')
+            {
+                Console.WriteLine("Podaj licznik ułamka: ");
+                string sx1 = Console.ReadLine();
+                int num;
+                while (!int.TryParse(sx1, out num) || sx1.Length == 0)
+                {
+                    Console.WriteLine("Nie podałeś liczby! Podaj jeszcze raz: ");
+                    sx1 = Console.ReadLine();
+                }
+                int ix1 = int.Parse(sx1);
+
+                Console.WriteLine("Podaj mianownik ułamka: ");
+                string sy1 = Console.ReadLine();
+                while (!int.TryParse(sy1, out num) || sy1.Length == 0)
+                {
+                    Console.WriteLine("Nie podałeś liczby! Podaj jeszcze raz: ");
+                    sy1 = Console.ReadLine();
+                }
+                int iy1 = int.Parse(sy1);
+
+                if (iy1 != 0)
+                {
+                    Fraction frac1 = new Fraction(ix1, iy1);
+                    int outcome = (int)frac1;
+
+                    Console.WriteLine("{0} po zamianie na liczbę całkowitą ma postać: {1}", frac1.toString(), outcome);
+                }
+                else
+                {
+                    Console.WriteLine("Podałeś 0 w mianowniku!");
+                }
+            }
+            else if(choice[0] == '8')
+            {
+                Console.WriteLine("Podaj liczbę całkowitą: ");
+                string sx1 = Console.ReadLine();
+                int num;
+                while (!int.TryParse(sx1, out num) || sx1.Length == 0)
+                {
+                    Console.WriteLine("Nie podałeś liczby! Podaj jeszcze raz: ");
+                    sx1 = Console.ReadLine();
+                }
+                int ix1 = int.Parse(sx1);
+
+                Fraction frac1 = ix1;
+
+                Console.WriteLine("{0} po zamianie na ułamek zwykły ma postać: {1}", ix1, frac1.toString());
+            }
+            else
+            {
+                Console.WriteLine("Błędne polecenie");
+            }
+            Console.WriteLine("Naciśnij esc, aby zakończyć lub dowolny inny klawisz aby powtórzyć.");
+            cki = Console.ReadKey();
+        } while (cki.Key != ConsoleKey.Escape);
     }
 }
